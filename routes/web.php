a393ed26dcab2a4eb068b305a72b61d3b258504b
KaiNizam <95922782+KaiNizam@users.noreply.github.com>
2023-01-14 23:35:30 +0800

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function(){
+    return view('homepage');
+});
+
+Route::get('/homepage', function(){
+    return view('homepage');
+});
+
+Route::get('/Manage Registration/RegNewMem', function(){
+    return view('Manage Registration/RegNewMem');
+});
+
+Route::get('/Manage Committee Election/Student/StudCandidateListPage', function () {
+    return view('Manage Committee Election/Student/StudCandidateListPage');
+});
+
+Route::get('/Manage Committee Election/Student/VotingPage', function () {
+    return view('Manage Committee Election/Student/VotingPage');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
